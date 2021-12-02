cb10b1575338f3e27b95852cb0005a717806eb53
Andrey Kulikov <9230697@gmail.com>
2021-12-02 11:15:56 +0300

    Добавил STEP

--- src/test/java/demoqa/tests/PracticeForm.java
+++ src/test/java/demoqa/tests/PracticeForm.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.*;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
 
 public class PracticeForm extends TestBase{
@@ -31,49 +32,61 @@ public class PracticeForm extends TestBase{
 
     @Test
     void registrationFormTest () {
-        $(".practice-form-wrapper")
-                .shouldHave(text("Student Registration Form"));
-        $("#firstName").setValue(firstname);
-        $("#lastName").setValue(lastName);
-        $("#userEmail").setValue(userEmail);
-        $(byText(gender)).click();
-        $("#userNumber").setValue(userNumber);
-
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption(monthOfBirth);
-        $(".react-datepicker__year-select").selectOption(yearOfBirth);
-        $("[aria-label='Choose Wednesday, August 3rd, 1988']").click();
-        $("#subjectsInput").val(subject).pressEnter();
-        $("#hobbiesWrapper").$(byText(hobby)).click();
+        step("Open and check students registration form", () ->
+            $(".practice-form-wrapper")
+                .shouldHave(text("Student Registration Form")));
+        step("Fill students registration form", () -> {
+                    step("Fill user data", () -> {
+                        $("#firstName").setValue(firstname);
+                        $("#lastName").setValue(lastName);
+                        $("#userEmail").setValue(userEmail);
+                        $(byText(gender)).click();
+                        $("#userNumber").setValue(userNumber);
+                    });
+                    step("Set birthdate", () -> {
+                        $("#dateOfBirthInput").click();
+                        $(".react-datepicker__month-select").selectOption(monthOfBirth);
+                        $(".react-datepicker__year-select").selectOption(yearOfBirth);
+                        $("[aria-label='Choose Wednesday, August 3rd, 1988']").click();
+                    });
+                    step("Set subjects", () ->
+                            $("#subjectsInput").val(subject).pressEnter());
+                    step("Set hobbies", () ->
+                            $("#hobbiesWrapper").$(byText(hobby)).click());
 //        $(byText(hobby)).click();
-        $("#uploadPicture").uploadFromClasspath("img/" + picture);
-        $("#currentAddress").val(currentAddress);
-        $("#state").scrollTo().click();
-        $("#stateCity-wrapper").$(byText(state)).click();
+                    step("Upload image", () ->
+                            $("#uploadPicture").uploadFromClasspath("img/" + picture));
+                    step("Set address", () -> {
+                        $("#currentAddress").val(currentAddress);
+                        $("#state").scrollTo().click();
+                        $("#stateCity-wrapper").$(byText(state)).click();
 //        $(byText(state)).click();
-        $("#city").click();
+                        $("#city").click();
 //        $(byText(city)).click();
-        $("#stateCity-wrapper").$(byText(city)).click();
-        $("#submit").click();
+                        $("#stateCity-wrapper").$(byText(city)).click();
+                    });
+                    step("Submit form", () ->
+                            $("#submit").click());
+                });
 
-        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").shouldHave(text(firstname + " " + lastName),
-                text(userEmail), text(gender));
+            step("Verify successful form submit", () -> {
+                $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+                $(".table-responsive").shouldHave(text(firstname + " " + lastName),
+                        text(userEmail), text(gender));
 
 //        Проверка данных во всплывающем окне
-        $(byText("Student Name")).parent()
-                .shouldHave(text(firstname + " " + lastName));
+                $(byText("Student Name")).parent()
+                        .shouldHave(text(firstname + " " + lastName));
 //        $x("//td[text()='Student Name']").parent().shouldHave(text(firstname + " " + lastName));
-        $x("//td[text()='Student Email']").parent().shouldHave(text(userEmail));
-        $x("//td[text()='Gender']").parent().shouldHave(text(gender));
-        $x("//td[text()='Mobile']").parent().shouldHave(text(userNumber));
-        $x("//td[text()='Date of Birth']").parent().shouldHave(text(dayOfBirth + " " + monthOfBirth + "," + yearOfBirth));
-        $x("//td[text()='Subjects']").parent().shouldHave(text(subject));
-        $x("//td[text()='Hobbies']").parent().shouldHave(text(hobby));
-        $x("//td[text()='Picture']").parent().shouldHave(text(picture));
-        $x("//td[text()='Address']").parent().shouldHave(text(currentAddress));
-        $x("//td[text()='State and City']").parent().shouldHave(text(state + " " + city));
-
-
+                $x("//td[text()='Student Email']").parent().shouldHave(text(userEmail));
+                $x("//td[text()='Gender']").parent().shouldHave(text(gender));
+                $x("//td[text()='Mobile']").parent().shouldHave(text(userNumber));
+                $x("//td[text()='Date of Birth']").parent().shouldHave(text(dayOfBirth + " " + monthOfBirth + "," + yearOfBirth));
+                $x("//td[text()='Subjects']").parent().shouldHave(text(subject));
+                $x("//td[text()='Hobbies']").parent().shouldHave(text(hobby));
+                $x("//td[text()='Picture']").parent().shouldHave(text(picture));
+                $x("//td[text()='Address']").parent().shouldHave(text(currentAddress));
+                $x("//td[text()='State and City']").parent().shouldHave(text(state + " " + city));
+            });
     }
 }
