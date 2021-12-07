a51a7fff9c2f384cf5cf3d44c2f99ab9e329a819
Andrey Kulikov <9230697@gmail.com>
2021-12-07 12:56:37 +0300

    поправил testbase

--- src/test/java/demoqa/properties/OwnerTests.java
+++ src/test/java/demoqa/properties/OwnerTests.java
@@ -4,6 +4,7 @@ import config.CredentialsConfig;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.*;
 
+import static io.qameta.allure.Allure.step;
 import static java.lang.String.format;
 
 @Tag("properties")
@@ -13,13 +14,15 @@ public class OwnerTests {
 
     @Test
     void readCredentialsTest() {
-        String login = credentials.login();
-        String password = credentials.password();
+        step("Берем данные из credentials.properties", () -> {
+            String login = credentials.login();
+            String password = credentials.password();
 
-        System.out.println(login);
-        System.out.println(password);
+            String message = format("https://%s:%s@selenoid.autotests.cloud/wd/hub/", login, password);
 
-        String message = format("i login as %s with password %s", login, password);
-        System.out.println(message);
+            System.out.println("Логин" + login);
+            System.out.println("Пароль" + password);
+            System.out.println("Адрес удаленного доступа:" + message);
+        });
     }
 }
--- src/test/java/demoqa/tests/TestBase.java
+++ src/test/java/demoqa/tests/TestBase.java
@@ -2,21 +2,32 @@ package demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.open;
+import static java.lang.String.format;
 
 public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        Configuration.startMaximized = true;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+
+        CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
+        String login = credentials.login();
+        String password = credentials.password();
+
+        String url = System.getProperty("url", "");
+        Configuration.remote = format("https://%s:%s@%s", login, password, url);
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+
+        String message = format("https://%s:%s@selenoid.autotests.cloud/wd/hub/", login, password);
+        System.out.println(message);
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
